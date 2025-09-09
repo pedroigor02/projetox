2895c00ee179c333eaab7e6de8d244287a59b620
pedroigor02 <pedroigorbrizolacarvalho@gmail.com>
2025-09-09 17:24:46 -0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    domains: ['blob.v0.dev'],
+  },
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+}
+
+export default nextConfig
